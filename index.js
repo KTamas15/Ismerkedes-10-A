035a003009ed16a3f5400ee9a4c947cbe6e9a024
KTam15 <kiss.tamas.zsolt@szbiszeged.hu>
2023-09-11 14:03:54 +0200

    első commit

---
+++ index.js
@@ -0,0 +1,49 @@
+let test = document.body;
+
+let parameter = document.createElement('p');
+parameter.innerText = ('Első parameter szoveg!');
+
+
+test.appendChild(parameter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+    alert("AZE");
+}
